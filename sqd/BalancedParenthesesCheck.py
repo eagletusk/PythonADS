ffeba85fa7dcab905bdddaa3569b493b4e70a93c
Paul Subzak <sobczak.paul@gmail.com>
2020-04-26 18:28:20 +0000

    BalancedParenthesesCheck.py added

--- sqd/BalancedParenthesesCheck.py
+++ sqd/BalancedParenthesesCheck.py
@@ -2,4 +2,21 @@ def balance_check(s):
   print('a')   
   pass
 
-balance_check('a')
+
+"""
+RUN THIS CELL TO TEST YOUR SOLUTION
+"""
+from nose.tools import assert_equal
+
+class TestBalanceCheck(object):
+    
+    def test(self,sol):
+        assert_equal(sol('[](){([[[]]])}('),False)
+        assert_equal(sol('[{{{(())}}}]((()))'),True)
+        assert_equal(sol('[[[]])]'),False)
+        print ('ALL TEST CASES PASSED')
+        
+# Run Tests
+
+t = TestBalanceCheck()
+t.test(balance_check)
