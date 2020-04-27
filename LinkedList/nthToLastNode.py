1ebbd331ec171fedc4215c61b84159ad8da32431
Paul Subzak <sobczak.paul@gmail.com>
2020-04-27 23:53:02 +0000

    added Nth to last added implementation

---
+++ LinkedList/nthToLastNode.py
@@ -0,0 +1,51 @@
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.nextnode  = None
+
+def nth_to_last_node(n, head):
+    # print(head.value)
+    next_node = head
+    prev_node = []
+    while next_node != None:
+      prev_node.append(next_node)
+      next_node = next_node.nextnode
+    
+    # print (prev_node)
+    lenn = len(prev_node)-1 
+    nth = lenn -n 
+    return prev_node[nth+1]
+
+
+"""
+RUN THIS CELL TO TEST YOUR SOLUTION AGAINST A TEST CASE 
+
+PLEASE NOTE THIS IS JUST ONE CASE
+"""
+
+from nose.tools import assert_equal
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+e = Node(5)
+
+a.nextnode = b
+b.nextnode = c
+c.nextnode = d
+d.nextnode = e
+
+####
+
+class TestNLast(object):
+    
+    def test(self,sol):
+        
+        assert_equal(sol(2,a),d)
+        print ('ALL TEST CASES PASSED')
+        
+# Run tests
+t = TestNLast()
+t.test(nth_to_last_node)
--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 # from sqd import Lambda
 # from sqd import nextIter
 # from sqd import Decorators
-from LinkedList import SLL
-from LinkedList import DLL
-from LinkedList import SLLcheck
-from LinkedList import LLreversal
+# from LinkedList import SLL
+# from LinkedList import DLL
+# from LinkedList import SLLcheck
+# from LinkedList import LLreversal
+from LinkedList import nthToLastNode
