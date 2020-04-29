74781aa2eb47b677d1129ce7dbd2cda347e879e3
Paul Subzak <sobczak.paul@gmail.com>
2020-04-29 02:11:58 +0000

    dynamic programming implementation

--- Recursion/fibonacci.py
+++ Recursion/fibonacci.py
@@ -1,11 +1,20 @@
 # Instantiate Cache information
 n = 10
-cache = [None] * (n + 1)
+cache = [None] * (n+1 )
 
 
 def fib_dyn(n):
-    
-    pass
+  # base
+  if n == 0 or n ==1:
+    return n
+  # check cache
+  if cache[n] != None:
+    return cache[n]
+  # keep seeting cache
+  cache[n] = fib_dyn(n-1) + fib_dyn(n-2)
+  
+  return cache[n]
+
 
 def fib_iter(n):
   a,b = 0,1
@@ -17,9 +26,17 @@ def fib_iter(n):
 
 
 def fib_rec(n):
+    # base case:
+    if n == 0 or n ==1:
+      return n
+
+    else: 
+      # print(n)
+      return fib_rec(n-1) +fib_rec(n-2)
+    # recursive case:
     
-    pass
 
 print(fib_rec(10))
 print(fib_dyn(10))
+print(cache)
 print(fib_iter(10))
