0280a65919a439842ae055b6fbe1f185db78fd5e
Paul Subzak <sobczak.paul@gmail.com>
2020-04-29 17:03:48 +0000

    preorder and postorder non class functions

--- Trees/nodeTree.py
+++ Trees/nodeTree.py
@@ -34,10 +34,34 @@ class BT(object):
   def getRootVal(self):
     return self.key
 
+  def preorder(self):
+    if self.key:
+      print(self.key)
+      if self.leftChild:
+        self.leftChild.preorder()
+      if self.rightChild:
+        self.rightChild.preorder()
+
+
 r = BT('a')
-print(r.getRootVal())
-print(r.getRightChild())
+# print(r.getRootVal())
+# print(r.getRightChild())
 r.insertLeft(3)
 r.insertLeft(2)
 r.insertRight(4)
-print(r.getLeftChild().getRootVal())
+# print(r.getLeftChild().getRootVal())
+
+r.preorder()
+
+
+def preorder(tree):
+  if tree:
+    print(tree.getRootVal())
+    preorder(tree.getLeftChild())
+    preorder(tree.getRightChild())
+
+def postorder(tree):
+  if tree:
+    postorder(tree.getLeftChild())
+    postorder(tree.getRightChild())
+    print(tree.getRootVal())
