9b14aec476fa54ed551a840ac194fd0054b996f5
Paul Subzak <sobczak.paul@gmail.com>
2020-05-01 01:24:50 +0000

    recursive search ...

--- Search/binarySearch.py
+++ Search/binarySearch.py
@@ -18,4 +18,31 @@ def binarySearch(arr,ele):
       l = mid +1
   return False
 
-print(binarySearch([3,4,5,56,78],78))
+# print(binarySearch([3,4,5,56,78],78))
+
+
+def rec_binarySearch(arr,ele):
+    
+  l= 0 
+  r = len(arr)-1
+
+  mid = (l+r)//2 
+
+  if len(arr) == 0:
+    print("false")
+    return False
+
+  if arr[mid] == ele:
+    print('true')
+    return True
+  
+  if ele < arr[mid]:
+    # print(arr[:mid-1])
+    rec_binarySearch(arr[:mid],ele)
+  if ele > arr[mid]:
+    # print(arr[mid+1:])
+    rec_binarySearch(arr[mid+1:],ele)    
+
+
+
+print(rec_binarySearch([3,4,5,56,78],78))
